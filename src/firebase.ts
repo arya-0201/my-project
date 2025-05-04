6f91e7f7301b7c41a7587ef5cfcd5eb268738687
박효준 <arya@nampyeon-isajunmaegbug.local>
2025-05-04 16:32:56 +0900

    서버접근 재시도2

--- src/firebase.ts
+++ src/firebase.ts
@@ -7,14 +7,14 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCNLuazytO2jgLc6gKUKE0oORNeL6OHKd0",
-  authDomain: "my-fridge-app-ebbb0.firebaseapp.com",
-  projectId: "my-fridge-app-ebbb0",
-  storageBucket: "my-fridge-app-ebbb0.firebasestorage.app",
-  messagingSenderId: "1019279680674",
-  appId: "1:1019279680674:web:0aa2279c22dbba807163fb",
-  measurementId: "G-H2E3BC4D6H"
-};
+    apiKey: "AIzaSyCNLuazytO2jgLc6gKUKE0oORNeL6OHKd0",
+    authDomain: "my-fridge-app-ebbb0.firebaseapp.com",
+    projectId: "my-fridge-app-ebbb0",
+    storageBucket: "my-fridge-app-ebbb0.firebasestorage.app",
+    messagingSenderId: "1019279680674",
+    appId: "1:1019279680674:web:0aa2279c22dbba807163fb",
+    measurementId: "G-H2E3BC4D6H"
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
